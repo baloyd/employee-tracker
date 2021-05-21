25ab5b314b1ae4ae1adf1ecc2ec6ebd626917a0f
baloyd <danielbaloy@gmail.com>
2021-05-21 13:47:15 -0400

    added starter info for each table

--- employee.sql
+++ employee.sql
@@ -5,7 +5,7 @@ CREATE DATABASE emp_trackDB;
 USE emp_trackDB;
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT, 
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NULL,
@@ -14,18 +14,49 @@ CREATE TABLE employee (
 );
 
 CREATE TABLE department(
-    id INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE role (
-    id INT PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
     PRIMARY KEY (id)
-)
+);
 
+INSERT INTO department 
+(name)
+VALUES
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO role
+(title, salary, department_id)
+VALUES
+('Sales Lead', 100000,1),
+('Salesperson',80000,1),
+('Lead Engineer',150000,2),
+('Software Engineer',120000,2),
+('Account Manager',180000,3),
+('Accountant',125000,3),
+('Legal Team Lead',250000,4),
+('Lawyer',180000,4);
+
+INSERT INTO employee
+(first_name,last_name,role_id,manager_id)
+VALUES
+('Gerard','Magimel', 1, NULL),
+('Joachim', 'Valentine', 2, 1),
+('Roger','Bacon', 3, NULL),
+('Alice','Elliot', 4, 3),
+('Karin','Koenig', 5, NULL),
+('Yuri','Hyuga', 6, 5),
+('Anastasia','Romanov', 7, NULL),
+('Hilda','Valentine', 8, 7)
 
 
